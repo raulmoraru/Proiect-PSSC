65658323db4177af4bb48abbb66dda3c0a008e74
raulmoraru <raul.moraru20@yahoo.com>
2022-11-19 12:18:31 +0200

    Created project

---
+++ Proiect PSSC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proiect_PSSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
