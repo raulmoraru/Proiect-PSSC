d73f89c653303e1dbdd212490359bb90870ca6c4
raulmoraru <raul.moraru20@yahoo.com>
2023-01-06 12:30:41 +0200

    Automated order id

--- ProiectPSSC/ProiectPSSC/Program.cs
+++ ProiectPSSC/ProiectPSSC/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using ProiectPSSC.Domain.Models;
 using static ProiectPSSC.Domain.Models.UnvalidatedOrder;
 
@@ -6,75 +7,17 @@ namespace ProiectPSSC
 {
     public class OrderWorkflow
     {
-        /*public void PlaceOrder(Order order)
-        {
-            ValidateOrder(order);
-            CalculateTotal(order);
-            ProcessPayment(order);
-            SaveOrder(order);
-            SendConfirmationEmail(order);
-        }
-        private void ValidateOrder(Order order)
-        {
-            // Validate the order details
-            // Throw an exception if the order is invalid
-            if (order.Quantity <= 0)
-            {
-                throw new Exception("Invalid quantity");
-            }
-        }
-        private void CalculateTotal(Order order)
-        {
-            // Calculate the total cost of the order
-            order.Total = order.Quantity * order.Price;
-        }
-        private void ProcessPayment(Order order)
-        {
-            // Process the payment for the order
-        }
-        private void SaveOrder(Order order)
-        {
-            // Save the order to the database
-        }
-        private void SendConfirmationEmail(Order order)
-        {
-            // Send a confirmation email to the customer
-        }
-    }
-    public class Order
-    {
-        public int Id { get; set; }
-        public string CustomerName { get; set; }
-        public string ItemName { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public decimal Total { get; set; }
-    }
-    */
 
         class Program
         {
+            private static int orderCounter=0;
+
             static void Main(string[] args)
             {
-                // Create an instance of the OrderWorkflow class
                 var workflow = new OrderWorkflow();
                 var OrdersList = ReadOrder().ToArray();
                 List<UnvalidatedOrder> OrderList_ = new List<UnvalidatedOrder>(OrdersList);
                 PrintOrders(OrderList_);
-               // PlaceOrderCommand command = new OrdersList();
-               /* var check = workflow.Execute(command, (checkExistence) => true);
-                check.Match(
-                     whenPlacingOrderEventFailedEvent: @event =>
-                     {
-                         Console.WriteLine($"The order could not be placed: {@event.Reason}");
-                         return @event;
-                     },
-                     whenPlacingOrderEventSucceededEvent: @event =>
-                     {
-                         Console.WriteLine($"The order was successfully placed");
-                         return @event;
-                     });
-                //workflow.PlaceOrder(order);*/
             }
 
             private static List<UnvalidatedOrder> ReadOrder()
@@ -82,11 +25,13 @@ namespace ProiectPSSC
                 List<UnvalidatedOrder> OrdersList = new();
                 do
                 {
-                    var orderId = ReadSomething("ID of the order: ");
-                    if (string.IsNullOrEmpty(orderId))
-                    {
-                        break;
-                    }
+                    /* var orderId = ReadSomething("ID of the order: ");
+                     if (string.IsNullOrEmpty(orderId))
+                     {
+                         break;
+                     }*/
+
+                    var orderId = (++orderCounter).ToString();
                     var CustomerName = ReadSomething("Name of the customer: ");
                     if (string.IsNullOrEmpty(CustomerName))
                     {
