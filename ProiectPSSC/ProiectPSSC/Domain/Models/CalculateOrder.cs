d04d9243277e382d8c4d26caf10a9f1716f12e89
oanaseracovanu <oanaseracovanu@outlook.com>
2023-01-11 23:11:31 +0200

    Delete ProiectPSSC/ProiectPSSC/Domain/Models directory

--- ProiectPSSC/ProiectPSSC/Domain/Models/CalculateOrder.cs
+++
@@ -1,7 +0,0 @@
-  using System;
-  
-  namespace ProiectPSSC.Domain.Models
-{
-    public record CalculateOrder(OredrId orderId, CustomerName CustomerName, ItemName ItemName, Quantity Quantity, Price Price);
-    
-}
